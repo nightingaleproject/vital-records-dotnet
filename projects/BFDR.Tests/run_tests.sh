a5854a74d4527cf905807d23c44a056cc4f716ad
MacAdam <smacadam@mitre.org>
2024-02-14 13:39:21 -0500

    fix(bfdr): add test file

--- projects/BFDR.Tests/run_tests.sh
+++ projects/BFDR.Tests/run_tests.sh
@@ -7,7 +7,7 @@ dotnet test BFDR.Tests/BirthRecord.Tests.csproj
 
 # Make sure we can read and parse a JSON file
 echo "* dotnet run --project BFDR.CLI json2json BFDR.Tests/fixtures/json/BasicBirthRecord.json"
-dotnet run --project BFDR.CLI json2json BFDR.Tests/fixtures/json/BasicBirthRecord.json > /dev/null
+dotnet run --project BFDR.CLI json2json BFDR.CLI/BasicBirthRecord.json > /dev/null
 
 # Make sure we can read and parse an XML file
 echo "* dotnet run --project BFDR.CLI xml2xml BFDR.Tests/fixtures/json/BasicBirthRecord.json"
