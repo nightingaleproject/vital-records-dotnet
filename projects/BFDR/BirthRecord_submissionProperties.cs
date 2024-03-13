51c409b32f77dcb62efdf9ff95c8e6412aecfeb7
Tricia Tran <38589916+ptran265@users.noreply.github.com>
2024-03-13 10:45:10 -0400

    fix(bfdr): add FHIRSubject annotation for InfantLiving and SSNRequested (#60)
    I checked all fields where GetOrCreateObservation or UpdateEntry (also
    calls subjectID()) was used. Using the debugger, I validated that the
    correct FHIR subjects were being assigned

--- projects/BFDR/BirthRecord_submissionProperties.cs
+++ projects/BFDR/BirthRecord_submissionProperties.cs
@@ -5817,6 +5817,7 @@ namespace BFDR
         /// </example>
         [Property("InfantLiving", Property.Types.Bool, "InfantLiving", "InfantLiving", false, BFDR.IGURL.ObservationInfantLiving, true, 288)]
         [FHIRPath("Bundle.entry.resource.where($this is Observation).where(code.coding.code='73757-7')", "")]
+        [FHIRSubject(FHIRSubject.Subject.Newborn)]
         public bool? InfantLiving
         {
             get
@@ -5961,6 +5962,7 @@ namespace BFDR
         /// </example>
         [Property("SSNRequested", Property.Types.Bool, "SSNRequested", "SSNRequested", false, BFDR.IGURL.ObservationSSNRequestedForChild, true, 288)]
         [FHIRPath("Bundle.entry.resource.where($this is Observation).where(code.coding.code='87295-2')", "")]
+        [FHIRSubject(FHIRSubject.Subject.Newborn)]
         public bool? SSNRequested
         {
             get
