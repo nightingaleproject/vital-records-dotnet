a0523091be1c3917f932ed428f0d3fe4e8caa9ec
rpassas <rpassas@mitre.org>
2024-04-10 16:40:36 -0400

    test(bfdr): adding identifiers tests
    fetalDeathRecord inherits from natalityRecord and uses the same fields used for natality
    tests verify the values can be set/get

---
+++ projects/BFDR.Tests/FetalDeathRecord_Should.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using VR;
+using Xunit;
+
+namespace BFDR.Tests
+{
+  public class FetalDeathRecord_Should
+  {
+    private FetalDeathRecord SetterFetalDeathRecord;
+
+    public FetalDeathRecord_Should()
+    {
+      SetterFetalDeathRecord = new FetalDeathRecord();
+    }
+
+    [Fact]
+    public void TestFetalDeathIdentifierSetters()
+    {
+      // Record Identifiers
+      SetterFetalDeathRecord.CertificateNumber = "87366";
+      Assert.Equal("87366", SetterFetalDeathRecord.CertificateNumber);
+      Assert.Equal("0000XX087366", SetterFetalDeathRecord.RecordIdentifier);
+      Assert.Null(SetterFetalDeathRecord.StateLocalIdentifier1);
+      SetterFetalDeathRecord.StateLocalIdentifier1 = "0000033";
+      Assert.Equal("0000033", SetterFetalDeathRecord.StateLocalIdentifier1);
+      SetterFetalDeathRecord.BirthYear = 2020;
+      SetterFetalDeathRecord.CertificateNumber = "767676";
+      Assert.Equal("2020XX767676", SetterFetalDeathRecord.RecordIdentifier);
+      SetterFetalDeathRecord.BirthLocationJurisdiction = "WY";
+      SetterFetalDeathRecord.CertificateNumber = "898989";
+      Assert.Equal("2020WY898989", SetterFetalDeathRecord.RecordIdentifier);
+    }
+  }
+}
