e542b4def2af6a7fbd19f9b1551e38a53e876d00
RobertScalfani <rscalfani@mitre.org>
2024-04-12 11:44:07 -0400

    Added date of registration tests

--- projects/BFDR.Tests/FetalDeathRecord_Should.cs
+++ projects/BFDR.Tests/FetalDeathRecord_Should.cs
@@ -33,5 +33,49 @@ namespace BFDR.Tests
       SetterFetalDeathRecord.CertificateNumber = "898989";
       Assert.Equal("0000WY898989", SetterFetalDeathRecord.RecordIdentifier);
     }
+
+    [Fact]
+    public void TestRegistrationDate()
+    {
+      FetalDeathRecord fhir = new FetalDeathRecord();
+      IJEFetalDeath ije = new IJEFetalDeath(fhir);
+      Assert.Equal("    ", ije.DOR_YR);
+      Assert.Equal("  ", ije.DOR_MO);
+      Assert.Equal("  ", ije.DOR_DY);
+      ije.DOR_DY = "24";
+      Assert.Equal("    ", ije.DOR_YR);
+      Assert.Equal("  ", ije.DOR_MO);
+      Assert.Equal("24", ije.DOR_DY);
+      ije.DOR_MO = "02";
+      Assert.Equal("    ", ije.DOR_YR);
+      Assert.Equal("02", ije.DOR_MO);
+      Assert.Equal("24", ije.DOR_DY);
+      ije.DOR_YR = "2023";
+      Assert.Equal("2023", ije.DOR_YR);
+      Assert.Equal("02", ije.DOR_MO);
+      Assert.Equal("24", ije.DOR_DY);
+    }
+
+    [Fact]
+    public void ParseRegistrationDate()
+    { 
+      FetalDeathRecord record = new(File.ReadAllText(TestHelpers.FixturePath("fixtures/json/FetalDeathReport.json")));
+      Assert.Equal("2019-01-09", record.RegistrationDate);
+      Assert.Equal(2019, record.RegistrationDateYear);
+      Assert.Equal(1, record.RegistrationDateMonth);
+      Assert.Equal(9, record.RegistrationDateDay);
+
+      IJEFetalDeath ije = new(record);
+      Assert.Equal("2019", ije.DOR_YR);
+      Assert.Equal("01", ije.DOR_MO);
+      Assert.Equal("09", ije.DOR_DY);
+
+      //set after parse
+      record.FirstPrenatalCareVisit = "2024-05"; 
+      Assert.Equal("2024-05", record.FirstPrenatalCareVisit);
+      Assert.Equal(2024, record.FirstPrenatalCareVisitYear);
+      Assert.Equal(5, record.FirstPrenatalCareVisitMonth);
+      Assert.Null(record.FirstPrenatalCareVisitDay);
+    }
   }
 }
