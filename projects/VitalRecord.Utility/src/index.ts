69f34afb8f5dcb4fcf7f9025fe81d118bdebe75d
Le Yang <le.yang@gmail.com>
2025-01-30 04:48:11 +0000

    try different port

--- projects/VitalRecord.Utility/src/index.ts
+++ projects/VitalRecord.Utility/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3005;
+const port = 8090;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express with TypeScript!');
