a0e30dc481c45e45697a47a4e345a77e5341030f
Le Yang <le.yang@gmail.com>
2025-01-30 05:06:59 +0000

    use 80

--- projects/VitalRecord.Utility/dist/index.js
+++ projects/VitalRecord.Utility/dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3005;
+const port = 80;
 app.get('/', (req, res) => {
     res.send('Hello from Express with TypeScript!');
 });
