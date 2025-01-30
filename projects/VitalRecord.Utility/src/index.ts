c10891d58c0a0dea1459007266d743173cc89415
Le Yang <le.yang@gmail.com>
2025-01-30 05:09:37 +0000

    use 80

--- projects/VitalRecord.Utility/src/index.ts
+++ projects/VitalRecord.Utility/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 8090;
+const port = 80;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express with TypeScript!');
