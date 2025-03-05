04c26f636ffb6e2260c9644ebfb1d896543d0c4b
RobertScalfani <rscalfani@mitre.org>
2025-03-05 13:21:09 -0500

    Updated error message

--- projects/VitalRecord/VitalRecord_constructors.cs
+++ projects/VitalRecord/VitalRecord_constructors.cs
@@ -100,7 +100,7 @@ namespace VR
             // Validate that the given Bundle is not a message.
             if (this.Bundle.Type == Bundle.BundleType.Message)
             {
-                throw new BundleTypeException("Expected a Vital Record Bundle, given a Message.");
+                throw new BundleTypeException("The FHIR Bundle must be of type Document, not Message.");
             }
         }
         
