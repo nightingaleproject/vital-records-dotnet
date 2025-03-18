95f1d503cf66425a95af87ecd5df3ca6f227eb6d
Saul Kravitz <skravitz@mitre.org>
2025-03-18 17:48:02 -0400

    Fix null AUXNO issue.  Should produce 12 blanks, not 12 '0'

--- projects/BFDR/IJEFetalDeath.cs
+++ projects/BFDR/IJEFetalDeath.cs
@@ -244,7 +244,7 @@ namespace BFDR
             {
                 if (String.IsNullOrWhiteSpace(record?.StateLocalIdentifier1))
                 {
-                    return "".PadLeft(12, '0');
+                    return (new String(' ', 12));
                 }
                 string auxNo = record.StateLocalIdentifier1;
                 if (auxNo.Length > 12)
