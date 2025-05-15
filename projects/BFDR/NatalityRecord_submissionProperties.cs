6ef48bf966134b0a08f8740259e7b339f9f44e0b
RobertScalfani <rscalfani@mitre.org>
2025-05-15 15:55:47 -0400

    Fix record-level birth date implementation

--- projects/BFDR/NatalityRecord_submissionProperties.cs
+++ projects/BFDR/NatalityRecord_submissionProperties.cs
@@ -373,7 +373,7 @@ namespace BFDR
             {
                 return null;
             }
-            return this.Subject.BirthDateElement.GetExtension(VR.ExtensionURL.PatientBirthTime).ToString();
+            return this.Subject.BirthDateElement.GetExtension(VR.ExtensionURL.PatientBirthTime).Value.ToString();
         }
 
         /// <summary>
@@ -382,7 +382,7 @@ namespace BFDR
         /// <param name="value"></param>
         protected void SetDateTimeOfDelivery(string value)
         {
-            FhirDateTime dateTime = ConvertToDateTime(value);
+            FhirDateTime dateTime = new FhirDateTime(value);
             this.Subject.BirthDateElement = ConvertToDate(value);
             this.Subject.BirthDateElement.SetExtension(VR.ExtensionURL.PatientBirthTime, dateTime);
         }
@@ -2228,7 +2228,7 @@ namespace BFDR
                 }
                 return this.Mother.BirthDate;
             }
-            set =>this.Mother.BirthDateElement = ConvertToDate(value);
+            set => this.Mother.BirthDateElement = ConvertToDate(value);
         }
 
         // Parent ages at delivery are represented as extensions on the child Patient resource as shown below
