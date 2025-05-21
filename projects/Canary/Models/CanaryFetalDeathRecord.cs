f80bda3ea0e4e340c1fad8f6a122d3fd4a16dbf8
Mint Thompson <mathompson@mitre.org>
2025-05-21 12:01:22 -0400

    refactor(canary): ije flag is named validate
    IJE constructors may be called with a boolean indicating whether to
    validate that the create object has no errors. Change name of boolean in
    Canary model functions that create an IJE to "validate" to match the IJE
    constructors.

--- projects/Canary/Models/CanaryFetalDeathRecord.cs
+++ projects/Canary/Models/CanaryFetalDeathRecord.cs
@@ -38,14 +38,14 @@ namespace canary.Models
             return new FetalDeathRecord();
         }
 
-        protected override VR.IJE CreateIJEFromRecord(VitalRecord record, bool permissive = true)
+        protected override VR.IJE CreateIJEFromRecord(VitalRecord record, bool validate = true)
         {
-            return new IJEFetalDeath((FetalDeathRecord)record, permissive);
+            return new IJEFetalDeath((FetalDeathRecord)record, validate);
         }
 
-        protected override VR.IJE CreateIJEFromString(string ije, bool permissive = true)
+        protected override VR.IJE CreateIJEFromString(string ije, bool validate = true)
         {
-            return new IJEFetalDeath(ije, permissive);
+            return new IJEFetalDeath(ije, validate);
         }
 
         protected override VitalRecord CreateRecordFromDescription(string value)
