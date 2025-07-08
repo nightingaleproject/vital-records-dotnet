846ca1b7f7d8ff98d36cf3de3e73a5ae8419b8e3
Marc Hadley <mhadley@mitre.org>
2025-07-08 16:45:02 -0400

    chore: fix build warnings (#260)
    closes: #NVSS-700
    
    ---------
    
    Co-authored-by: RobertScalfani <rscalfani@mitre.org>

--- projects/Canary.Tests/ConnectathonControllerTests.cs
+++ projects/Canary.Tests/ConnectathonControllerTests.cs
@@ -4,6 +4,7 @@ using VRDR;
 using BFDR;
 using System.IO;
 using Newtonsoft.Json;
+using System;
 
 namespace canary.tests
 {
@@ -31,7 +32,13 @@ namespace canary.tests
             var response = controller.IndexBFDR();
             var result = controller.IndexBFDR();
             BirthRecord romeroConnectathon = result[0];
-            Assert.Equal(JsonConvert.SerializeObject(br), JsonConvert.SerializeObject(romeroConnectathon));
+            // The timezone has to be manually set here because the connectathon records are generated based on the local time zone. However, our test FHIR records have hard coded time zone data which must be updated to match the local time zone.
+            string timeZoneOffset = TimeZoneInfo.Local.GetUtcOffset(new DateTime(2000, 1, 1)).ToString()[..6];
+            if (timeZoneOffset == "00:00:")
+            {
+                timeZoneOffset = "+00:00";
+            }
+            Assert.Equal(JsonConvert.SerializeObject(br).Replace("-05:00", timeZoneOffset), JsonConvert.SerializeObject(romeroConnectathon));
         }
     }
 }
